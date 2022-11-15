f6e6a779de68d10550a8b3a2bb0bba52d3fb2687
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2022-11-15 11:43:17 +0300

    Лабораторна №1 Консоль

--- Main.java
+++ Main.java
@@ -1,17 +1,50 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[5];
-        int sum = 0;
-        int count = 0;
+        int[] array = new int[7];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
-            System.out.println(array[i]);
-            if (array[i] % 2 == 0) {
+            array[i] = (int) (Math.random() * 201) - 100;
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Сума від'ємних елементів: " + sum(array));
+        System.out.println("Добуток елементів, розтащованих між мінімальним та максимальним елементами: " + product(array));
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
                 sum += array[i];
-                count++;
             }
         }
-        System.out.println("Середній арифметичний індекс парних елементів масиву: " + (double) sum / count);
+        return sum;
+    }
+
+    public static int product(int[] array) {
+        int product = 1;
+        int min = array[0];
+        int max = array[0];
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        if (minIndex > maxIndex) {
+            int temp = minIndex;
+            minIndex = maxIndex;
+            maxIndex = temp;
+        }
+        for (int i = minIndex + 1; i < maxIndex; i++) {
+            product *= array[i];
+        }
+        return product;
     }
 }
